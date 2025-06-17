164a7328563d3649533448e40e96258c506f36df
trev-git <htaedshararam@gmail.com>
2025-06-17 16:36:21 +0300

    asd

--- setup-web.sh
+++ setup-web.sh
@@ -4,6 +4,8 @@ if [ $(id -u) -ne 0 ]; then
     echo 'You need to be root to setup the server!' && exit 1
 fi
 
+mkdir -p /srv/http/player
+
 cp -rf ./config-httpserver/* /srv/http/
 chmod 666 /srv/http/*.txt
 chmod 777 /srv/http/player
